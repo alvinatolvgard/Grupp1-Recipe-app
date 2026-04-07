fcb0dcf9c0ccd83105edd4d48e521cee07b47c5a
Sanel Osmanovic <osmanovicsanel@hotmail.com>
2026-04-07 18:30:29 +0200

    Design för receptkort klart enligt Figma. Lite småfix kvar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './css/App.css'
+import RecipeCard from './components/RecipeCard/RecipeCard'; // Sanel receptkort
 
 function App() {
-
   return (
-    <>
-     
-    </>
-  )
+    <div style={{ padding: '40px', backgroundColor: '#F9F5F2', minHeight: '100vh' }}>
+      <h1 style={{ fontFamily: 'Georgia, serif', marginBottom: '30px', }}>Alla recept</h1>
+      <RecipeCard />
+    </div>
+  );
 }
 
-export default App
+export default App;
+
