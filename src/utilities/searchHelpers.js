3e87b51b923905392f0cd3f5ce4a89e091704050
alvina <alvinatolvgard@gmail.com>
2026-04-09 08:35:51 +0200

    add comments

--- src/utilities/searchHelpers.js
+++ src/utilities/searchHelpers.js
@@ -1,6 +1,14 @@
+/**
+ * Filtrerar en lista med recept baserat på kategori
+ * @author Alvina
+ * @param {Array} recipes - listan med recept från API:t
+ * @param {string} activeFilters - valt kategori, tex "dinner" eller "all"
+ * @returns filtrerad lista med recept
+ */
+
 function filterRecipes(recipes, activeFilters) {
     if (!recipes) return [];
-    
+
     if (activeFilters === "all") {
         return recipes;
     } else {
