b9b0b3fbf276302d2213bbb580e822c22e39da41
Alvina Tolvgård <alvinatolvgard@gmail.com>
2026-04-21 11:23:27 +0200

    Clean up App component by removing RecipeCard and state
    Removed unused imports and state management for recipes.

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,11 @@ import './App.css';
 import RecipeDetailPage from './pages/RecipeDetailPage';
 import Footer from './components/Footer/Footer'; {/* lades till */}
 
-// ta bort ---
-import RecipeCard from './components/RecipeCard/RecipeCard'
-import { useState, useEffect } from 'react'
-// ---
-
 function App() {
-  // ta bort ---
-  const [recipe, setRecipe] = useState(null)
-
-useEffect(() => {
-  fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-    .then(res => res.json())
-    .then(data => setRecipe(data.meals[0]))
-}, [])
-// ----
 
   return (
     <BrowserRouter>
       <Routes>
-        {/* ta bort */}
-        <Route path='/' element={recipe && <RecipeCard recipe={recipe} />} />
-        {/* --- */}
         <Route path='/recipe/:id' element={<RecipeDetailPage />} />
       </Routes>
       <Footer /> {/* lades till */}
