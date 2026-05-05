12e9e70e6bb632a6565ba6675c8cdb9710f47c80
Leo <albrim_@hotmail.com>
2026-05-05 18:35:21 +0200

    lägg till about-sida

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,46 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter, Routes, Route, ScrollRestoration } from "react-router-dom";
+import "./App.css";
 
 // Sidor
-import LandingPage from './pages/LandingPage';
-import RecipeDetailPage from './pages/RecipeDetailPage';
-import LoginPage from './pages/LoginPage'; // Logga in sidan
-import ProfilePage from './pages/ProfilePage'; //Profil sidan
+import LandingPage from "./pages/LandingPage";
+import RecipeDetailPage from "./pages/RecipeDetailPage";
+import Favourites from "./pages/Favourites";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import About from './pages/About';
 
-//Komponenter
+// Komponenter
 import Header from "./components/Header/Header";
-import Footer from './components/Footer/Footer'; {/* lades till */}
-import ProtectedRoute from './components/ProtectedRoute'; //Skydd för sidor som kräver inlogg
-
-
-
+import Footer from "./components/Footer/Footer";
+import Toast from "./components/Toast/Toast";
+import ProtectedRoute from "./components/ProtectedRoute";
+import ScrollToTopButton from "./components/ScrollToTopButton";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      <Toast />
+      <ScrollToTopButton />
+
       <Routes>
-        <Route path='/' element={<LandingPage />} />
-        <Route path='/recipe/:id' element={<RecipeDetailPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/profile' element={
-          <ProtectedRoute>
-            <ProfilePage />
-          </ProtectedRoute>
-        }
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/recipe/:id" element={<RecipeDetailPage />} />
+        <Route path="/favourites" element={<Favourites />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path='/about' element={<About />} />
+
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <ProfilePage />
+            </ProtectedRoute>
+          }
         />
       </Routes>
-      <Footer /> {/* lades till */}
+
+      <Footer />
     </BrowserRouter>
   );
 }
